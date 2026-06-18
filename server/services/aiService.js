05b1abab81d75ec7799d2b468e6ca86761db65bd
Aditya Kumar <adityatechspace@gmail.com>
2026-06-18 15:15:50 +0530

    Set gemini at 2.5

--- server/services/aiService.js
+++ server/services/aiService.js
@@ -31,7 +31,7 @@ export const askPortfolioAssistant = async (
   userQuestion
 ) => {
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
+    model: "gemini-2.5-flash",
   });
 
 const prompt = `
